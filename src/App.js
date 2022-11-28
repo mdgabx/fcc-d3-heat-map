fa461e130cdae1fd57c146d9fdad8009c1d632f4
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2022-11-28 09:35:47 +0800

    passing the data to heatmap.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import HeatMap from "./components/heatMap";
 
 function App() {
+  const [heatData, setHeatData] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            setHeatData(data);
+        })
+
+  }, [setHeatData])
+
   return (
     <div className="App">
-      <HeatMap />
+      <HeatMap dataset={heatData}/>
     </div>
   );
 }
--- src/components/heatMap.jsx
+++ src/components/heatMap.jsx
@@ -1,9 +1,14 @@
-const HeatMap = () => {
+const HeatMap = ({ dataset }) => {
+    console.log(dataset);
+
     return ( 
         <main className="container">
             <div className="chart">
                 <h1 id="title">Monthly Global Land-Surface Temperature</h1>
                 <p id="description">1753 - 2015: base temperature 8.66℃</p>
+                <div className="render">
+                    { /*  renders the data chart here  */ }
+                </div>
             </div>
         </main>
     );
