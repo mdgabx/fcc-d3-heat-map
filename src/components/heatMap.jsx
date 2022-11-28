38d663d941dea3203e1be34389d299e06c4d9ccd
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2022-11-28 09:18:40 +0800

    adding the html and css

---
+++ src/components/heatMap.jsx
@@ -0,0 +1,12 @@
+const HeatMap = () => {
+    return ( 
+        <main className="container">
+            <div className="chart">
+                <h1 id="title">Monthly Global Land-Surface Temperature</h1>
+                <p id="description">1753 - 2015: base temperature 8.66℃</p>
+            </div>
+        </main>
+    );
+}
+ 
+export default HeatMap;
